4b54db6f73a257311ce0c6631ce1bbeba0736c32
Gareth Smyth <ghbsmyth@gmail.com>
2016-01-20 23:34:57 +0000

    Adding some a fake response from /programs

--- server/Controller.cs
+++ server/Controller.cs
@@ -6,11 +6,55 @@ using Nancy;
 
 namespace achiir6500.server
 {
-    class Controller : NancyModule
+    public class Controller : NancyModule
     {
         public Controller()
         {
-            Get["/programs"] = _ => "Hi";
+            Get["/programs"] = _ => @"{
+                [
+                    ""program"" : { // max programs 10 (0-9)
+                        ""id"": 0,
+                        ""loop_cunter"": ""1"", // 1-200 
+                        ""steps"": { // max steps 8????????????????????
+                            ""step"": {
+                                ""ramp_rate"" : ""0.01"",
+                                ""target"": ""25"",
+                                ""dwell"": ""9999""
+                            },
+                            ""step"": {
+                                ""ramp_rate"" : ""99.99"",
+                                ""target"": ""30"",
+                                ""dwell"": ""9999""
+                            },
+                            ""step"": {
+                                ""ramp_rate"" : ""END"",
+                                ""target"": """",
+                                ""dwell"": """"
+                            }
+                        }
+                    },
+                        ""program"" : {
+                        ""id"": 1,
+                        ""steps"": {
+                            ""step"": {
+                                ""ramp_rate"" : ""1"",
+                                ""target"": ""180"",
+                                ""dwell"": ""30""
+                            },
+                            ""step"": {
+                                ""ramp_rate"" : ""1"",
+                                ""target"": ""210"",
+                                ""dwell"": ""35""
+                            },
+                            ""step"": {
+                                ""ramp_rate"" : ""END"",
+                                ""target"": """",
+                                ""dwell"": """"
+                            }
+                        }
+                    }
+                ]
+            }";
         }
     }
 }
