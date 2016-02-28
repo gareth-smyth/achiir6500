6adea1e73cee87df529feeaf7f0c6085e54c911b
Gareth Smyth <ghbsmyth@gmail.com>
2016-02-28 19:24:04 +0000

    Adding all the port settings needed to communicate with the reflow station

--- server/ReflowStation.cs
+++ server/ReflowStation.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using Newtonsoft.Json.Linq;
 
 namespace achiir6500.server
@@ -12,6 +14,12 @@ namespace achiir6500.server
         {
             System.IO.Ports.SerialPort myPort = new System.IO.Ports.SerialPort("COM1");
             myPort.BaudRate = 9600;
+            myPort.DtrEnable = true;
+            myPort.RtsEnable = true;
+            myPort.ReadBufferSize = 8;
+            myPort.DataBits = 7;
+            myPort.Parity = Parity.Even;
+            myPort.StopBits = StopBits.One;
 
             if (myPort.IsOpen == false) 
                 myPort.Open();
@@ -19,16 +27,19 @@ namespace achiir6500.server
             var pc900Translator = new Pc900Translator();
 
             Console.WriteLine("Sending start command");
-            var startCommand = pc900Translator.StartCommand(program.id);
+            var startCommand = pc900Translator.StartCommand("1");
             foreach (var command in startCommand.CommandsList)
             {
                 byte[] buffer = command.ToArray();
-                Console.WriteLine("Sent:" + string.Join(", ", buffer));
+                
                 myPort.Write(buffer, 0, buffer.Length);
-
-                byte ackByte = Convert.ToByte(myPort.ReadByte());
-                Console.WriteLine("Received:"+ackByte);
-                startCommand.ResponseDelegate(new List<byte> {ackByte});
+                Console.WriteLine("Sent:" + string.Join(", ", buffer));
+                Console.WriteLine("Receiving bytes");
+                int bytes = myPort.BytesToRead;
+                Console.WriteLine("Receiving bytes:"+bytes);
+                byte[] readBuffer = new byte[bytes];
+                myPort.Read(readBuffer, 0, bytes);
+                startCommand.ResponseDelegate(readBuffer.ToList());
             }
             
             myPort.Close();
