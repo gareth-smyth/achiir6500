70b15c5129ba5f39080a7edb8a4b9782379b3ad1
Gareth Smyth <ghbsmyth@gmail.com>
2016-03-28 13:43:35 +0100

    Changing the graphing library to sparklines as graphjs struggled with the series

--- web_gui/app/program_chart.js
+++ web_gui/app/program_chart.js
@@ -1,78 +1,59 @@
 var React = require('react');
-var LineChart = require("react-chartjs").Line;
+var Sparklines = require('react-sparklines').Sparklines;
+var SparklinesLine = require('react-sparklines').SparklinesLine;
 
 module.exports = React.createClass({
+    config: {
+        secondsPerTick: 5
+    },
+
     // Conversions between programs and chart data
-    setChartData: function(dataSet, program){
+    generateChartData: function (program) {
         var data = [];
 
         var stepStartingTemp = 0;
-        program.steps.forEach(function(currentStep, index){
+        program.steps.forEach((currentStep, index) => {
             var tempDifference = currentStep.level - stepStartingTemp;
 
             var timeToLevel = (tempDifference / currentStep.ramp);
 
-            for(var rampSecondsPassed = 0; rampSecondsPassed<timeToLevel; rampSecondsPassed+=20){
+            for (var rampSecondsPassed = 0; rampSecondsPassed < timeToLevel; rampSecondsPassed += this.config.secondsPerTick) {
                 var temperature = stepStartingTemp + (rampSecondsPassed * (tempDifference / timeToLevel));
                 data.push(temperature);
             }
 
-            for(var dwellSecondsPassed = 0; dwellSecondsPassed<currentStep.dwell; dwellSecondsPassed+=20){
+            for (var dwellSecondsPassed = 0; dwellSecondsPassed < currentStep.dwell; dwellSecondsPassed += this.config.secondsPerTick) {
                 data.push(currentStep.level);
             }
 
             stepStartingTemp = currentStep.level;
         });
 
-        dataSet.data = data;
+        return data;
     },
 
     // Component methods
     getInitialState: function () {
         return {
-            chartData: {
-                labels: [],
-                datasets: [
-                    {
-                        label: "Program",
-                        fillColor: "rgba(220,220,220,0.2)",
-                        strokeColor: "rgba(220,220,220,1)",
-                        pointColor: "rgba(220,220,220,1)",
-                        pointStrokeColor: "#fff",
-                        pointHighlightFill: "#fff",
-                        pointHighlightStroke: "rgba(220,220,220,1)",
-                        data: []
-                    }
-                ]
-            }
-        };
+            chartData: []
+        };  
     },
 
     componentWillReceiveProps: function (nextProps) {
-        var currentChartData = this.state.chartData;
-        var selectedIndex = nextProps.programs.findIndex(function(program){
-           return nextProps.selectedProgram === program.id;
+        var selectedIndex = nextProps.programs.findIndex(function (program) {
+            return nextProps.selectedProgram === program.id;
         });
         selectedIndex = selectedIndex < 0 ? 0 : selectedIndex;
-        this.setChartData(
-            currentChartData.datasets[0],
-            nextProps.programs[selectedIndex]);
-        currentChartData.labels = [];
-        this.updateState(this.state, currentChartData);
-    },
-
-    updateState: function (currentState, chartData) {
-        this.setState(
-            {
-                chartData: chartData
-            });
+        this.state.chartData = this.generateChartData(nextProps.programs[selectedIndex]);
+        this.setState(this.state);
     },
 
     render: function () {
-        var chartOptions = {responsive: true, maintainAspectRatio: false};
-
         return (<div className={"chartdiv"}>
-            <LineChart data={this.state.chartData} options={chartOptions}/>
+            <Sparklines data={this.state.chartData} containerWidth={"100%"} containerHeight={"100%"}
+                        width={300} height={100} margin={5} preserveAspectRatio={"none"}>
+                <SparklinesLine />
+            </Sparklines>
         </div>);
     }
 });
